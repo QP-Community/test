fd4b583fd0d1b75d273ffec69403706a6b5a55f7
MaxlaserTechAlt <pakorn.angpao@gmail.com>
2024-06-03 23:52:14 +0700

    Update universal.lua

--- modules/universal.lua
+++ modules/universal.lua
@@ -1,15 +1,9 @@
-cloneref = cloneref or function(obj) return obj end
-playerService = cloneref(game.FindService(game, 'Players'))
-lplr = playerService.LocalPlayer
-promptService = cloneref(game:FindService('ProximityPromptService'))
-replicatedStorage = cloneref(game:FindService('ReplicatedStorage'))
-
-gui = loadstring(game.HttpGet(game, 'https://raw.githubusercontent.com/MaxlaserTechAlt/funny-script/main/core/gui.lua'))()
-win = gui:CreateWindow({
-    Name = 'QP Scripts',
+local gui = loadstring(game.HttpGet(game, 'https://raw.githubusercontent.com/MaxlaserTechAlt/funny-script/main/core/gui.lua'))()
+local win = gui:CreateWindow({
+    Name = 'QP Script',
     Theme = game.FindService(game, 'HttpService'):JSONEncode(readfile('QP/theme.json').theme) or 'Luna'
 })
-tabs = {
+local tabs = {
     Main = win.CreateTab('Main'),
     Settings = win.CreateTab('Settings')
 }
@@ -20,7 +14,7 @@ shared.tabs = tabs
 local theme = {
     theme = ''
 }
-themechanger = tabs.Settings.CreateSection('Theme')
+local themechanger = tabs.Settings.CreateSection('Theme')
 themechanger.CreateDropdown({
     Name = 'Theme',
     List = {'DarkTheme', 'LightTheme', 'BloodTheme', 'GrapeTheme', 'Ocean', 'Midnight', 'Sentinel', 'Vape', 'Luna', 'Private', 'Synapse', 'Serpent'},
